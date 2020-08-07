fa53c66c395c5be46d1c0bd2361d53c79f1bb46c
KleybsonFerreira <kleybson.ferreira.web@gmail.com>
2020-08-07 10:47:58 -0300

    add-nunjucks

--- src/server.js
+++ src/server.js
@@ -1,18 +1,51 @@
+const proffys = [
+    {
+        name:"Kleybson Ferreira",
+        avatar:"https://avatars0.githubusercontent.com/u/68382633?s=460&u=0710552a592fb51f9fb0f310732e6af3fb5ae234&v=4",
+        whatsapp:"81985055255",
+        bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject:"Química",
+        cost:"20",
+        weekday:[0],
+        time_from:[720],
+        time_to:[1220]
+        },
+
+    { name:"Diego Fernandes",
+            avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+            whatsapp:"81985055255",
+            bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+            subject:"Química",
+            cost:"20",
+            weekday:[6],
+            time_from:[720],
+            time_to:[1220]
+        }
+]
+//função que vai chamar a pagina principal
+function pageLanding(req, res){
+    return res.sendFile(__dirname + "/views/index.html")
+}
+//função que vai chamar a pagina study
+function pageStudy(req, res){
+    return res.sendFile(__dirname + "/views/study.html")
+}
+//função que vai chamar a pagina give-classes
+function pageGiveClasses(req, res){
+    return res.sendFile(__dirname + "/views/give-classes.html")
+}
+
+
+
+
 const express = require('express')
 const server = express()
 
 server.use(express.static("public"))
-.get("/", (req, res) => {
+//config de arquivos estaticos
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
 
-    return res.sendFile(__dirname + "/views/index.html")
-})
-.get("/study", (req, res) => {
-
-    return res.sendFile(__dirname + "/views/study.html")
-})
-.get("/give-classes", (req, res) => {
-
-    return res.sendFile(__dirname + "/views/give-classes.html")
-})
 .listen(5500)
 
